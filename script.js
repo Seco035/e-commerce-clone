395c4a81e88074a3d8574474d516ae4ec403b110
Selcukibar <selcukibarr@icloud.com>
2023-09-19 15:00:02 +0300

    texts

--- script.js
+++ script.js
@@ -19,11 +19,11 @@ function ekranaYazdir(resim){
     row.innerHTML += `
          <div class="col-3 my-3">
                      <div class="card h-100 d-flex align-items-center pt-5 text-center">
-                     <img src=${resim.image} alt=""  height="250px" width="250px" class="card-img-top pt-3" alt="">
+                     <img src=${resim.image} alt=""  height="250px" width="250px" class="card-img-top p-4" alt="">
                      <div class="card-body">
                          <h5 class="card-title">${resim.title}</h5>
                          <h5 class="card-title fs-3" style="color: green;">${resim.price} ₺</h5>
-                         <a id="addBtn" href="#" style="background-color: rgb(196, 194, 194);" class="btn"><i id="addBtn" class="bi bi-cart4 text-black">Add To Cart</i></a>
+                         <a id="addBtn" href="#" style="background-color: rgb(196, 194, 194);" class="btn">Add To Cart</a>
                      </div>
                  </div>
              </div>
