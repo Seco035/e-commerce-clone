2f2fc80fe10a83b8a322a840d9454bb000f700e1
Selcukibar <selcukibarr@icloud.com>
2023-09-19 14:26:01 +0300

    sepete ekle ikonunun artması

--- script.js
+++ script.js
@@ -19,43 +19,24 @@ function ekranaYazdir(resim){
     row.innerHTML += `
          <div class="col-3 my-3">
                      <div class="card h-100 d-flex align-items-center pt-5 text-center">
-                     <img src=${resim.image} alt="" class="card-img-top w-50 pt-3" alt="">
+                     <img src=${resim.image} alt=""  height="250px" width="250px" class="card-img-top pt-3" alt="">
                      <div class="card-body">
                          <h5 class="card-title">${resim.title}</h5>
                          <h5 class="card-title fs-3" style="color: green;">${resim.price} ₺</h5>
-                         <a id="addBtn" href="#" class="btn btn-secondary">Sepete Ekle</a>
+                         <a id="addBtn" href="#" style="background-color: rgb(196, 194, 194);" class="btn"><i id="addBtn" class="bi bi-cart4 text-black">Add To Cart</i></a>
                      </div>
                  </div>
              </div>
          `
 }
 
-
-// document.addEventListener("DOMContentLoaded", function(){
-//     fetch(url)
-//     .then(function(response){
-//         return response.json()
-//     })
-//     .then(function(data){
-//         console.log(data)
-//         data.forEach(function(resim){
-//             ekranaYazdir(resim)
-//         })
-//     })
-// })
-// const row = document.querySelector(".row");
-// function ekranaYazdir(resim){
-
-//     row.innerHTML += `
-//     <div class="col-4">
-//                 <div class="card">
-//                 <img src=${resim.image} alt="" class="card-img-top" alt="">
-//                 <div class="card-body">
-//                     <h5 class="card-title">${resim.title}</h5>
-//                     <h5 class="card-title">${resim.price} TL</h5>
-//                     <a id="addBtn" href="#" class="btn btn-success">Sepete Ekle</a>
-//                 </div>
-//             </div>
-//         </div>
-//     `
-// }
+// sepete ekleye bastığımızda sepetin içindeki ürün sayısını arttıran kodlar.
+row.addEventListener("click", ekle)
+function ekle(e){
+    if(e.target.id.includes("addBtn")){
+        const parentDiv = e.target.parentElement.parentElement
+        console.log(parentDiv);
+        const cart = document.querySelector(".cart-box")
+        cart.innerHTML++;
+    }
+}
