258edf0f6a0cae5dfa5c8b4f52ff298e95964333
Selcukibar <selcukibarr@icloud.com>
2023-09-22 11:40:07 +0300

    contact başlangıç

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ document.addEventListener("DOMContentLoaded", function(){
 
 // ekranaYazdir(resim) fonksiyonunu tanimliyoruz.
 const row = document.querySelector(".rowW");
-function ekranaYazdir(resim){
+function ekranaYazdir(photo){
     row.innerHTML += `
          <div class="col-4 my-3">
                      <div class="card h-100 d-flex align-items-center text-center">
-                     <img src=${resim.image} alt=""  height="250px" width="250px" class="card-img-top p-4" alt="">
+                     <img src=${photo.image} alt=""  height="250px" width="250px" class="card-img-top p-4" alt="">
                      <div class="card-body">
-                         <h5 class="card-title">${resim.title}</h5>
-                         <h5 class="card-title fs-3" style="color: green;">${resim.price} ₺</h5>
+                         <h5 class="card-title">${photo.title}</h5>
+                         <h5 class="card-title fs-3" style="color: green;">${photo.price} ₺</h5>
                          <a id="addBtn" href="#" style="background-color: rgb(196, 194, 194);" class="btn">Add To Cart</a>
                      </div>
                  </div>
